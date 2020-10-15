b1c6ad248c7ee53af6ee4c6cf4e2acf04a984fa6
ArtcalO97 <niyca14@gmail.com>
2020-10-15 20:51:26 +0200

    Main files py for the project

---
+++ ijambo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+# Create your views here.
+
+class HomeView(View):
+	template_name = 'index.html'
+
+	def get(self, request, *args, **kwargs):
+		return render(request, self.template_name, locals())
