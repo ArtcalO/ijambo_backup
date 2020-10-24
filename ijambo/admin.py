7adc0fb6efb3ad3c6a49fe9193a79e021b41e275
ArtcalO97 <niyca14@gmail.com>
2020-10-24 16:09:12 +0200

    register mprofil model in admin .py

--- ijambo/admin.py
+++ ijambo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+class ProfilAdmin(admin.ModelAdmin):
+    list_display = ('user', 'avatar')
+    list_filter = ('user', 'avatar')
+    search_fields = ('user', 'avatar')
+
+admin.site.register(Profil, ProfilAdmin)
