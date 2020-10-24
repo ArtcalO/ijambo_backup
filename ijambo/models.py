0e0ef864abf74b3c8a68a73bea4c4c3a67878b16
ArtcalO97 <niyca14@gmail.com>
2020-10-24 16:09:23 +0200

    Profil model added

--- ijambo/models.py
+++ ijambo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
+from . models import *
 
-# Create your models here.
+class Profil(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True, blank=True, upload_to="profil/avatars/")
+    
+    def __str__(self):
+        return f"{self.user.username}"
