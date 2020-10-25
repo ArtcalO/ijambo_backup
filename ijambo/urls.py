590e087af95c968cd6180d8323a71c8613f742fd
ArtcalO97 <niyca14@gmail.com>
2020-10-25 20:30:17 +0100

    urls resolving

--- ijambo/urls.py
+++ ijambo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.HomeView.as_view()),
-    path('register/', views.register,name='register')
+    path('', views.HomeView.as_view(), name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.connexion, name='login'),
+    path('logout/', views.deconexion, name='logout'),
 ]
