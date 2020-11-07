64e181c5c86d19317c29bd5fff1b774a91d527cb
ArtcalO97 <niyca14@gmail.com>
2020-11-07 15:12:28 +0100

    Handling Anonymous issue

--- ijamboAdmin/views.py
+++ ijamboAdmin/views.py
@@ -4,5 +4,10 @@ from ijambo.models import *
 # Create your views here.
 
 def home(request):
-	profile = Profil.objects.get(user=request.user)
-	return render(request, 'index2.html', locals())
+	try:
+		profile = Profil.objects.get(user=request.user)
+		return render(request, 'simple_user.html', locals())
+	except Exception as e:
+		err_msg = "You must login first to access this page !"
+		return render(request, '404.html', locals())
+	
